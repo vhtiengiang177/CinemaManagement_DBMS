5c052170ffeaec59f023389f8c11dbbd1448a3fd
Nguyễn Gia Bảo Lê <lenguyengiabao46@outlook.com>
2020-11-11 01:34:46 +0700

    Customer

--- CinemaManagement/CinemaManagement/Program.cs
+++ CinemaManagement/CinemaManagement/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CinemaManagement.Customer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace CinemaManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fEmployee());
+            Application.Run(new CustomerForm());
         }
     }
 }

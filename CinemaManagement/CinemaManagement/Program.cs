9e1da452838321912eddf26fad37ce5c80596ddd
yenlee38 <ngocyen174308@gmail.com>
2020-11-11 21:05:32 +0700

    thay doi program

--- CinemaManagement/CinemaManagement/Program.cs
+++ CinemaManagement/CinemaManagement/Program.cs
@@ -16,7 +16,7 @@ namespace CinemaManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fEmployee());
+            Application.Run(new fMain());
         }
     }
 }
